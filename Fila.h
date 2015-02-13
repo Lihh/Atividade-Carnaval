0c5acb3123aa19fa198f8913a8b12f1f2edff661
Lidia Chou <lidia.chou94@gmail.com>
2015-02-13 12:04:15 -0200

    arquivos

---
+++ Fila.h
@@ -0,0 +1,13 @@
+//
+//  Fila.h
+//  Genius
+//
+//  Created by Lidia Chou on 2/13/15.
+//  Copyright (c) 2015 Lidia Chou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Fila : NSObject
+
+@end
---
+++ Fila.m
@@ -0,0 +1,41 @@
+//
+//  Fila.m
+//  Fila
+//
+//  Created by Fernando Lucheti on 06/02/15.
+//  Copyright (c) 2015 Fernando Lucheti. All rights reserved.
+//
+
+#import "Fila.h"
+
+@implementation Fila
+
+-(id)init {
+    self = [super init];
+    elementos = [[NSMutableArray alloc]init];
+    
+    return self;
+}
+-(void) enfileirar:(NSObject *)elem {
+    [elementos addObject: elem];
+}
+-(void) desenfileirar{
+    [elementos removeObjectAtIndex: 0];
+}
+
+-(NSObject *) ler{
+    if (! [self vazio]){
+      return elementos.firstObject;
+    }else{
+        return nil;
+    }
+}
+-(BOOL) vazio{
+    if (elementos.count == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+@end
